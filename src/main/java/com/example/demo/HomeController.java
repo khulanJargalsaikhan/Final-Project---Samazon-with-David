a1f96eebd7c753fbaa6dbaf5a6edf833b45b57b7
khulanJargalsaikhan <onagraa@gmail.com>
2021-08-17 09:27:51 -0400

    included home page

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -19,25 +22,12 @@ public class HomeController {
     RoleRepository roleRepository;
 
     @Autowired
-    ProductRepository productRepository;
-
-    @Autowired
-    CartRepository cartRepository;
-
-    @Autowired
     CloudinaryConfig cloudc;
 
     @RequestMapping("/")
-    public String index(Model model){
-        model.addAttribute("products", productRepository.findAll());
-        return "index";
+    public String homePage(){
+        return "home";
     }
-
-//    @RequestMapping("/addToCart/{id}")
-//    public String addToCart(@PathVariable("id") long id){
-//       Product product = productRepository.findById(id).get();
-//
-//    }
 
     @RequestMapping("/login")
     public String login(){
