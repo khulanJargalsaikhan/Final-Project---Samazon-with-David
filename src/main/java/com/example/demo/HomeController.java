06287566394663404181665ad5d5e932b368a27e
khulanJargalsaikhan <onagraa@gmail.com>
2021-08-17 11:22:55 -0400

    with flowers page

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -24,10 +21,36 @@ public class HomeController {
     @Autowired
     CloudinaryConfig cloudc;
 
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CartRepository cartRepository;
+
     @RequestMapping("/")
     public String homePage(){
         return "home";
     }
+
+    @RequestMapping("/flowers")
+    public String showAllFlowers(Model model){
+        model.addAttribute("products", productRepository.findAll());
+        model.addAttribute("carts", cartRepository.findAll());
+        return "flowers";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String showShoppingCart(@PathVariable("id") long id, Model model){
+        model.addAttribute("product", productRepository.findById(id).get());
+        model.addAttribute("carts", cartRepository.findAll());
+        return "shoppingcart";
+    }
+
+
+
+
+
+
 
     @RequestMapping("/login")
     public String login(){
